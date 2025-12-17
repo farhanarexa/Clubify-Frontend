a68341f2c0c2a3f14d916cfcf85c3ff34133f26b
farhanarexa <mailfarhana702@gmail.com>
2025-12-17 16:56:21 +0600

    navbar and footer logo updated

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
                     {/* Company Info */}
                     <div className="space-y-4">
                         <div className="flex items-center space-x-3">
-                            <div className="w-12 h-12 rounded-full bg-linear-to-r from-[#A45CFF] to-[#7ED8FF] flex items-center justify-center shadow-lg">
-                                <span className="text-white font-bold text-xl">C</span>
+                            <div className=" rounded-full bg-linear-to-r from-[#A45CFF] to-[#7ED8FF] flex items-center justify-center shadow-lg">
+                                <img className='h-22' src="https://i.postimg.cc/JhrQ3PRb/people-2368650.png" alt="" />
                             </div>
                             <h3 className="text-2xl font-bold">Clubify</h3>
                         </div>
@@ -21,39 +21,39 @@ const Footer = () => {
                         </p>
                         <div className="flex space-x-4 mt-4">
                             <a
-                                href="#"
+                                href="https://www.facebook.com/"
                                 className="text-gray-300 hover:text-white hover:bg-[#A45CFF] hover:bg-opacity-30 p-2 rounded-full transition-all duration-300"
                                 aria-label="Facebook"
                             >
-                                <FaFacebook size={18} />
+                                <FaFacebook size={20} />
                             </a>
                             <a
-                                href="#"
+                                href="https://x.com/"
                                 className="text-gray-300 hover:text-white hover:bg-[#A45CFF] hover:bg-opacity-30 p-2 rounded-full transition-all duration-300"
                                 aria-label="Twitter"
                             >
-                                <FaTwitter size={18} />
+                                <FaTwitter size={20} />
                             </a>
                             <a
-                                href="#"
+                                href="https://www.instagram.com/"
                                 className="text-gray-300 hover:text-white hover:bg-[#A45CFF] hover:bg-opacity-30 p-2 rounded-full transition-all duration-300"
                                 aria-label="Instagram"
                             >
-                                <FaInstagram size={18} />
+                                <FaInstagram size={20} />
                             </a>
                             <a
-                                href="#"
+                                href="https://www.linkedin.com"
                                 className="text-gray-300 hover:text-white hover:bg-[#A45CFF] hover:bg-opacity-30 p-2 rounded-full transition-all duration-300"
                                 aria-label="LinkedIn"
                             >
-                                <FaLinkedin size={18} />
+                                <FaLinkedin size={20} />
                             </a>
                             <a
-                                href="#"
+                                href="https://github.com/"
                                 className="text-gray-300 hover:text-white hover:bg-[#A45CFF] hover:bg-opacity-30 p-2 rounded-full transition-all duration-300"
                                 aria-label="GitHub"
                             >
-                                <FaGithub size={18} />
+                                <FaGithub size={20} />
                             </a>
                         </div>
                     </div>
@@ -63,7 +63,7 @@ const Footer = () => {
                         <h4 className="text-lg font-bold mb-6 text-white relative pb-2 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-12 after:h-0.5 after:bg-linear-to-r after:from-[#A45CFF] after:to-[#7ED8FF]">
                             Quick Links
                         </h4>
-                        <ul className="space-y-3">
+                        <ul className="space-y-1">
                             <li>
                                 <Link to="/" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
                                     Home
@@ -75,12 +75,12 @@ const Footer = () => {
                                 </Link>
                             </li>
                             <li>
-                                <Link to="#" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
+                                <Link to="/about" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
                                     About Us
                                 </Link>
                             </li>
                             <li>
-                                <Link to="#" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
+                                <Link to="/contact" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
                                     Contact
                                 </Link>
                             </li>
@@ -97,7 +97,7 @@ const Footer = () => {
                         <h4 className="text-lg font-bold mb-6 text-white relative pb-2 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-12 after:h-0.5 after:bg-linear-to-r after:from-[#A45CFF] after:to-[#7ED8FF]">
                             Popular Categories
                         </h4>
-                        <ul className="space-y-3">
+                        <ul className="space-y-1">
                             <li>
                                 <Link to="/availableclubs?category=photography" className="text-gray-300 hover:text-white  transition-colors duration-300 block py-1">
                                     Photography
@@ -131,7 +131,7 @@ const Footer = () => {
                         <h4 className="text-lg font-bold mb-6 text-white relative pb-2 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-12 after:h-0.5 after:bg-linear-to-r after:from-[#A45CFF] after:to-[#7ED8FF]">
                             Stay Connected
                         </h4>
-                        <p className="text-gray-200 mb-4">
+                        <p className="text-gray-200 mb-3">
                             Subscribe to our newsletter for the latest updates and exclusive offers.
                         </p>
                         <div className="space-y-4">
@@ -151,7 +151,7 @@ const Footer = () => {
                                     Subscribe
                                 </button>
                             </form>
-                            <p className="text-gray-400 text-sm mt-2">
+                            <p className="text-gray-300 text-sm mt-2">
                                 We respect your privacy. Unsubscribe at any time.
                             </p>
                         </div>
@@ -159,7 +159,7 @@ const Footer = () => {
                 </div>
 
                 {/* Divider */}
-                <div className="border-t border-white/20 my-8"></div>
+                <div className="border-t border-white/20 my-5"></div>
 
                 {/* Bottom Bar */}
                 <div className="flex flex-col md:flex-row justify-between items-center">
